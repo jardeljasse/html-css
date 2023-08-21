6a1b126371991f7f559c8a0783f20917ef30f75e
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2023-08-21 12:29:05 +0200

    form with gua

---
+++ guanabara/notepad++/modulo-4/form.js
@@ -0,0 +1,12 @@
+
+const elIname = document.getElementById('iname')
+const elIdate = document.getElementById('idate')
+const elIfile = document.getElementById('ifile')
+
+const validationForm = () => {
+	
+	if(elIname.value = ''){
+		alert('Name, is required!')
+	}
+	
+}
